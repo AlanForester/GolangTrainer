faf448dfde41499348443d9a5cd4a67f771ce98e
Alex Collin <alex@collin.su>
2020-10-02 11:19:26 +0300

    Fix tests

--- handlers_test.go
+++ handlers_test.go
@@ -10,20 +10,15 @@ import (
 )
 
 func TestSadPath(t *testing.T) {
-	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
-	// pass 'nil' as the third parameter.
 
 	req, err := http.NewRequest("GET", "/api/slow", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
 	rr := httptest.NewRecorder()
 	handler := defaultHandler()
 
-	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
-	// directly and pass in our Request and ResponseRecorder.
 	handler.ServeHTTP(rr, req)
 
 	// Check the status code is what we expect.
